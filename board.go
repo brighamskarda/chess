185ebebaa3bda70fd56e79fa61fab19d3e7e16d4
Brigham Skarda <brighamskarda@gmail.com>
2025-04-13 00:01:16 -0600

    make basic board functions

--- board.go
+++ board.go
@@ -22,6 +22,31 @@ const DefaultFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 // The zero value is usable, though not very useful. You likely will want to use the following instead:
 // 		chess.ParseFEN(DefaultFEN)
 type Board struct {
+	whitePawns   Bitboard
+	whiteRooks   Bitboard
+	whiteKnights Bitboard
+	whiteBishops Bitboard
+	whiteQueens  Bitboard
+	whiteKings   Bitboard
+
+	blackPawns   Bitboard
+	blackRooks   Bitboard
+	blackKnights Bitboard
+	blackBishops Bitboard
+	blackQueens  Bitboard
+	blackKings   Bitboard
+
+	sideToMove Color
+
+	whiteKsCastle bool
+	whiteQsCastle bool
+	blackKsCastle bool
+	blackQsCastle bool
+
+	enPassant Square
+
+	halfMove uint
+	fullMove uint
 }
 
 // ParseFEN returns an error if it could not parse an FEN. It was likely malformed or missing important pieces.
@@ -42,82 +67,165 @@ func (b *Board) PrettyString(whitesPerspective bool, extraInfo bool) string {
 }
 
 func (b *Board) SideToMove() Color {
-	return White
+	return b.sideToMove
 }
 
 func (b *Board) SetSideToMove(c Color) {
+	b.sideToMove = c
 }
 
 // WhiteKingSideCastle returns true if white may still castle kingside. Note that this does not indicate if the move is currently valid. It is really an indication of if the king or rook have moved yet this game.
 func (b *Board) WhiteKingSideCastle() bool {
-	return true
+	return b.whiteKsCastle
 }
 
 // WhiteQueenSideCastle returns true if white may still castle queenside. Note that this does not indicate if the move is currently valid. It is really an indication of if the king or rook have moved yet this game.
 func (b *Board) WhiteQueenSideCastle() bool {
-	return true
+	return b.whiteQsCastle
 }
 
 // BlackKingSideCastle returns true if black may still castle kingside. Note that this does not indicate if the move is currently valid. It is really an indication of if the king or rook have moved yet this game.
 func (b *Board) BlackKingSideCastle() bool {
-	return true
+	return b.blackKsCastle
 }
 
 // BlackQueenSideCastle returns true if black may still castle queenside. Note that this does not indicate if the move is currently valid. It is really an indication of if the king or rook have moved yet this game.
 func (b *Board) BlackQueenSideCastle() bool {
-	return true
+	return b.blackQsCastle
 }
 
 func (b *Board) SetWhiteKingSideCastle(value bool) {
+	b.whiteKsCastle = value
 }
 
 func (b *Board) SetWhiteQueenSideCastle(value bool) {
+	b.whiteQsCastle = value
 }
 
 func (b *Board) SetBlackKingSideCastle(value bool) {
+	b.blackKsCastle = value
 }
 
 func (b *Board) SetBlackQueenSideCastle(value bool) {
+	b.blackQsCastle = value
 }
 
 // EnPassant returns the square on to which a pawn may move to perform en-passant. This does not indicate if the move is legal. NoSquare is returned if there is no en passant option.
 func (b *Board) EnPassant() Square {
-	return NoSquare
+	return b.enPassant
 }
 
 func (b *Board) SetEnPassant(s Square) {
-
+	b.enPassant = s
 }
 
 func (b *Board) HalfMove() uint {
-	return 0
+	return b.halfMove
 }
 
 func (b *Board) SetHalfMove(i uint) {
-
+	b.halfMove = i
 }
 
 func (b *Board) FullMove() uint {
-	return 0
+	return b.fullMove
 }
 
 func (b *Board) SetFullMove(i uint) {
-
+	b.fullMove = i
 }
 
 // Piece gets the piece on the given square. NoPiece is returned if no piece is present.
 func (b *Board) Piece(s Square) Piece {
+	if b.whitePawns.Square(s) == 1 {
+		return WhitePawn
+	}
+	if b.whiteRooks.Square(s) == 1 {
+		return WhiteRook
+	}
+	if b.whiteKnights.Square(s) == 1 {
+		return WhiteKnight
+	}
+	if b.whiteBishops.Square(s) == 1 {
+		return WhiteBishop
+	}
+	if b.whiteQueens.Square(s) == 1 {
+		return WhiteQueen
+	}
+	if b.whiteKings.Square(s) == 1 {
+		return WhiteKing
+	}
+
+	if b.blackPawns.Square(s) == 1 {
+		return BlackPawn
+	}
+	if b.blackRooks.Square(s) == 1 {
+		return BlackRook
+	}
+	if b.blackKnights.Square(s) == 1 {
+		return BlackKnight
+	}
+	if b.blackBishops.Square(s) == 1 {
+		return BlackBishop
+	}
+	if b.blackQueens.Square(s) == 1 {
+		return BlackQueen
+	}
+	if b.blackKings.Square(s) == 1 {
+		return BlackKing
+	}
+
 	return NoPiece
 }
 
 // SetPiece sets the given piece at the given square.
 func (b *Board) SetPiece(p Piece, s Square) {
+	b.ClearPiece(s)
 
+	switch p {
+	case WhitePawn:
+		b.whitePawns = b.whitePawns.SetSquare(s)
+	case WhiteRook:
+		b.whiteRooks = b.whiteRooks.SetSquare(s)
+	case WhiteKnight:
+		b.whiteKnights = b.whiteKnights.SetSquare(s)
+	case WhiteBishop:
+		b.whiteBishops = b.whiteBishops.SetSquare(s)
+	case WhiteQueen:
+		b.whiteQueens = b.whiteQueens.SetSquare(s)
+	case WhiteKing:
+		b.whiteKings = b.whiteKings.SetSquare(s)
+
+	case BlackPawn:
+		b.blackPawns = b.blackPawns.SetSquare(s)
+	case BlackRook:
+		b.blackRooks = b.blackRooks.SetSquare(s)
+	case BlackKnight:
+		b.blackKnights = b.blackKnights.SetSquare(s)
+	case BlackBishop:
+		b.blackBishops = b.blackBishops.SetSquare(s)
+	case BlackQueen:
+		b.blackQueens = b.blackQueens.SetSquare(s)
+	case BlackKing:
+		b.blackKings = b.blackKings.SetSquare(s)
+	}
 }
 
 // ClearPiece removes any piece from the given square.
 func (b *Board) ClearPiece(s Square) {
+	b.whitePawns = b.whitePawns.ClearSquare(s)
+	b.whiteRooks = b.whiteRooks.ClearSquare(s)
+	b.whiteKnights = b.whiteKnights.ClearSquare(s)
+	b.whiteBishops = b.whiteBishops.ClearSquare(s)
+	b.whiteQueens = b.whiteQueens.ClearSquare(s)
+	b.whiteKings = b.whiteKings.ClearSquare(s)
 
+	b.blackPawns = b.blackPawns.ClearSquare(s)
+	b.blackRooks = b.blackRooks.ClearSquare(s)
+	b.blackKnights = b.blackKnights.ClearSquare(s)
+	b.blackBishops = b.blackBishops.ClearSquare(s)
+	b.blackQueens = b.blackQueens.ClearSquare(s)
+	b.blackKings = b.blackKings.ClearSquare(s)
 }
 
 // IsCheck returns true if the side to move has a king under attack from an enemy piece.
@@ -150,4 +258,4 @@ func (b *Board) Move(m Move) {
 
 }
 
-// TODO implement all these functions.
+// TODO implement Move, IsCheck, PrettyString, String, ParseFEN.
