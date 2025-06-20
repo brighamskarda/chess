9d3d60623e2b5bf8d48c32b2a876d375b7ae79b8
Brigham Skarda <brighamskarda@gmail.com>
2025-06-20 15:21:28 -0600

    provide package description

---
+++ doc.go
@@ -0,0 +1,17 @@
+// Copyright (C) 2025 Brigham Skarda
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// chess is a package the provides pgn parsing, bitboard utilities, and performant move generation for the game of chess.
+package chess
