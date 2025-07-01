b3e867ed7a1258b82c2a095f6c2291e6e52efd27
Brigham Skarda <brighamskarda@gmail.com>
2025-07-01 15:06:56 -0600

    implement read loop
    New clients read continuously from stdout and stderr

--- uci/client_test.go
+++ uci/client_test.go
@@ -15,7 +15,12 @@
 
 package uci
 
-import "testing"
+import (
+	"io"
+	"strings"
+	"testing"
+	"time"
+)
 
 func TestNewClient_ErrorOnInvalidBinary(t *testing.T) {
 	_, err := NewClient("./dkfdks.exe", ClientSettings{})
@@ -28,5 +33,97 @@ func TestNewClient_NoErrorOnValidBinary(t *testing.T) {
 	_, err := NewClient(dummyBinaryPath, ClientSettings{})
 	if err != nil {
 		t.Errorf("%v", err)
+	}
+}
+
+type clientProgramMock struct {
+	stdinReader  *io.PipeReader
+	stdinWriter  *io.PipeWriter
+	stdoutReader *io.PipeReader
+	stdoutWriter *io.PipeWriter
+	stderrReader *io.PipeReader
+	stderrWriter *io.PipeWriter
+}
+
+func (cp *clientProgramMock) Terminate() error {
+	return nil
+}
+func (cp *clientProgramMock) Kill() error {
+	cp.stdinReader.Close()
+	cp.stdinWriter.Close()
+	cp.stdoutReader.Close()
+	cp.stdoutWriter.Close()
+	cp.stderrReader.Close()
+	cp.stderrWriter.Close()
+	return nil
+}
+func (cp *clientProgramMock) Wait() error {
+	return nil
+}
+func (cp *clientProgramMock) Write(p []byte) (int, error) {
+	return cp.stdinWriter.Write(p)
+}
+func (cp *clientProgramMock) Read(p []byte) (int, error) {
+	return cp.stdoutReader.Read(p)
+}
+func (cp *clientProgramMock) ReadErr(p []byte) (int, error) {
+	return cp.stderrReader.Read(p)
+}
+
+func newDummyClientProgram() *clientProgramMock {
+	stdinReader, stdinWriter := io.Pipe()
+	stdoutReader, stdoutWriter := io.Pipe()
+	stderrReader, stderrWriter := io.Pipe()
+	return &clientProgramMock{
+		stdinReader:  stdinReader,
+		stdinWriter:  stdinWriter,
+		stdoutReader: stdoutReader,
+		stdoutWriter: stdoutWriter,
+		stderrReader: stderrReader,
+		stderrWriter: stderrWriter,
+	}
+}
+
+func TestClient_StdoutToLogger(t *testing.T) {
+	dummyClient := newDummyClientProgram()
+	defer dummyClient.Kill()
+	testLogger := strings.Builder{}
+	_, err := newClientFromClientProgram(dummyClient, ClientSettings{Logger: &testLogger})
+	if err != nil {
+		t.Fatalf("couldn't make client: %v", err)
+	}
+
+	dummyClient.stdoutWriter.Write([]byte("line1\n"))
+	dummyClient.stdoutWriter.Write([]byte("line2"))
+	dummyClient.stdoutWriter.Write([]byte("line3\n"))
+
+	time.Sleep(10 * time.Millisecond)
+
+	expected := "<<< line1\n<<< line2line3\n"
+	loggerOutput := testLogger.String()
+	if loggerOutput != expected {
+		t.Errorf("logger output does not match: expected %v, got %v", expected, loggerOutput)
+	}
+}
+
+func TestClient_StderrToLogger(t *testing.T) {
+	dummyClient := newDummyClientProgram()
+	defer dummyClient.Kill()
+	testLogger := strings.Builder{}
+	_, err := newClientFromClientProgram(dummyClient, ClientSettings{Logger: &testLogger})
+	if err != nil {
+		t.Fatalf("couldn't make client: %v", err)
+	}
+
+	dummyClient.stderrWriter.Write([]byte("line1\n"))
+	dummyClient.stderrWriter.Write([]byte("line2"))
+	dummyClient.stderrWriter.Write([]byte("line3\n"))
+
+	time.Sleep(10 * time.Millisecond)
+
+	expected := "!<! line1\n!<! line2line3\n"
+	loggerOutput := testLogger.String()
+	if loggerOutput != expected {
+		t.Errorf("logger output does not match: expected %v, got %v", expected, loggerOutput)
 	}
 }
